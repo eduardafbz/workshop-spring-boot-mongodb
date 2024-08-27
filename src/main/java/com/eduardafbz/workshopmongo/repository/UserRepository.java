7cf6912db712bddf385828dfe81acd840e837c6b
eduardafbz <mariaeduardafbraz@outlook.com>
2024-08-27 13:44:14 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/eduardafbz/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.eduardafbz.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eduardafbz.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+	
+	
+}
