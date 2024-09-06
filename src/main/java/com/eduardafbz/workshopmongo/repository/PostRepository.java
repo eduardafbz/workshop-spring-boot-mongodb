b3ec6de0021c2d8c9a1fc25b2635059d97298dca
Maria Eduarda de Faria Braz Maria <mariaeduardafbraz@outlook.com>
2024-09-06 16:32:36 -0300

    Consulta simples com query methods

--- src/main/java/com/eduardafbz/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/eduardafbz/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.eduardafbz.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.eduardafbz.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
